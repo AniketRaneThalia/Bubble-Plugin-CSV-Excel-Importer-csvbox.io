28cb8ede80610e214ab96ee1963ce7a3e7a62f62
AniketRaneThalia <87179045+AniketRaneThalia@users.noreply.github.com>
2023-04-26 13:52:50 +0530

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,76 +1,70 @@
-function(instance, properties, context) {           
-   
+function(instance, properties, context) {
     
-      $(document).ready(function() {         
-        
-     	document.addEventListener("import_done", function eventHandler(e) {
+    $(document).ready(function() {
+
+        if(CSVBoxImporter) {
             
-            //console.log(e.detail);
-            if(e.detail.import_status == "success")
-                {
-            		instance.triggerEvent('import_success', function(){return console.log('success');});        
-                }
-            else
-                {
-                    instance.triggerEvent('import_fail', function(){return console.log('fail');});        
-                }
+            let userObj = {
+				'user_id': properties.user_id
+            };
             
- 			 
-    	});
-        
-        //console.log('ready');          
-          
-          div = window.u.div('', instance.canvas,'');
-            div.css('width','100%');
-            div.css('height','100%');             
-    
-				// creating button element
-                var button = document.createElement('BUTTON');
-                 
-                // creating text to be
-                //displayed on button
-                var text = document.createTextNode(properties.button_text);
-                 
-                // appending text to button
-                button.appendChild(text);
-                button.style.width= '100%';
-                button.style.height= '100%';
-          		button.className += properties.button_classes;   
-          		button.disabled = 'disabled';
-          		button.setAttribute('data-csvbox', '');                
-                 
-                // appending button to div
-                div.append(button);          		
-          
-          var userobj = {'user_id': properties.user_id};
-          
-          if(properties.custom_attribute_2_key)
-              {
-                  userobj[properties.custom_attribute_2_key] = properties.custom_attribute_2_value;
-              }
-          
-           if(properties.custom_attribute_3_key)
-              {
-                  userobj[properties.custom_attribute_3_key] = properties.custom_attribute_3_value;
-              }
-          
-           if(properties.custom_attribute_4_key)
-              {
-                  userobj[properties.custom_attribute_4_key] = properties.custom_attribute_4_value;
-              }
-          
-           if(properties.custom_attribute_5_key)
-              {
-                  userobj[properties.custom_attribute_5_key] = properties.custom_attribute_5_value;
-              }
-          
-          initializeImporter(properties.license_key, userobj, button);
-          
-          div.click(function() {
-              openModal();
-            });  
-           
-                                 
-	});    
-   
+            if (properties.custom_attribute_2_key) {
+            	userObj[properties.custom_attribute_2_key] = properties.custom_attribute_2_value;
+            }
+            
+            if (properties.custom_attribute_2_key) {
+            	userObj[properties.custom_attribute_2_key] = properties.custom_attribute_2_value;
+            }
+
+            if (properties.custom_attribute_3_key) {
+                userObj[properties.custom_attribute_3_key] = properties.custom_attribute_3_value;
+            }
+
+            if (properties.custom_attribute_4_key) {
+                userObj[properties.custom_attribute_4_key] = properties.custom_attribute_4_value;
+            }
+
+            if (properties.custom_attribute_5_key) {
+                userObj[properties.custom_attribute_5_key] = properties.custom_attribute_5_value;
+            }
+
+            let importer = new CSVBoxImporter(properties.license_key, userObj, function(result, data) {
+                if(result) {
+                    instance.triggerEvent('import_success');
+                }else{
+                    instance.triggerEvent('import_fail');
+                }
+            });
+
+            importer.setUser({
+                user_id: 'default123'
+            });
+
+            var button = document.createElement('BUTTON');
+            var text = document.createTextNode(properties.button_text);
+
+            button.appendChild(text);
+            button.style.width = '100%';
+            button.style.height = '100%';
+            button.className += properties.button_classes;
+            button.disabled = 'disabled';
+            button.setAttribute('data-csvbox', '');
+
+			instance.canvas.empty();
+            
+            instance.canvas.append(button)
+
+            importer.listen("onReady", function() {
+                if(button) button.removeAttribute('disabled');
+                else console.log("button not initialised")
+            });
+            
+            button.addEventListener("click", function(){
+                importer.openModal();
+            });
+
+        }
+
+    });
+
 }
